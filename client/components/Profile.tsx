48239cf7fde82bcd29e2baf69915eb8730ca6f74
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-05-07 09:54:56 +1200

    profile refinements

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -1,33 +1,35 @@
 function Profile() {
   return (
     <>
-      <div className="flex min-h-[40vh] px-5 py-12 md:flex-row flex-col items-center text-white bg-zinc-600 justify-between">
-        <div className="basis-2/3 min-h-[40vh] md:border-r-4 px-2 border-gray-300 text-base  lg:flex-grow md:w-1/2  self-center">
+      <div className="flex min-h-[40vh] px-5 py-12 md:flex-row flex-col items-start text-white bg-slate-900 justify-between">
+        <div className="basis-2/3 min-h-[40vh] md:border-r-4 px-2 border-gray-300 text-base lg:flex-grow md:w-1/2 ">
           <h2 className="text-5xl py-12 md:pt-0 bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-pink-500 font-light text-center">
             About Me
           </h2>
           <p className="text-slate-200 text-lg font-medium font-sans">
-            <span className="min-h-[20vh]">I</span>&apos;m Ryan and I&apos;m a
-            web developer with a background in technical support and the social
-            sciences. In the past few years I have been getting back to my
-            technical roots, exploring what a more technical career path might
-            look like. It was when I began helping out with the websites of a
-            think-tank at Victoria University on a contract basis that I found
-            what I was looking for. I was so fascinated by the work of my
-            predecessor that I began taking coding courses. When I finally
-            managed to clone down the website to start experimenting with my own
-            code, it dawned on me that I had found what I was looking for. Dev
-            Academy seemed like natural progression. This process of upskilling
-            and orienting my career towards progressively more technical
-            pursuits has been a blast, and I am looking to continue that
-            journey.
+            <p className="text-2xl pb-2">
+              I&apos;m Ryan and I&apos;m a web developer with a background in
+              technical support and the social sciences.
+            </p>
+            <br></br>
+            In the past few years I have been getting back to my technical
+            roots, exploring what a more technical career path might look like.
+            It was when I began helping out with the websites of a think-tank at
+            Victoria University on a contract basis that I found what I was
+            looking for. I was so fascinated by the work of my predecessor that
+            I began taking coding courses. When I finally managed to clone down
+            the website to start experimenting with my own code, it dawned on me
+            that I had found what I was looking for. Dev Academy seemed like
+            natural progression. This process of upskilling and orienting my
+            career towards progressively more technical pursuits has been a
+            blast, and I am looking to continue that journey.
           </p>
         </div>
-        <div className="min-h-[40vh] basis-1/3 relative text-base  lg:flex-grow md:w-1/2  divide-l-4 self-center">
+        <div className="min-h-[40vh] basis-1/3 relative text-base lg:flex-grow md:w-1/2  divide-l-4">
           <h2 className="text-5xl py-12 md:pt-0 bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-pink-500 font-light text-center	">
             Ryan Online
           </h2>
-          <ul className="pt-12 list-none text-white text-lg pl-2">
+          <ul className="py-12 list-none text-white text-lg pl-2">
             <li className="flex items-center">
               <img
                 className="basis 1/10 inline-block w-4 h-4 rounded-full ring-1 ring-white"
@@ -71,10 +73,10 @@ function Profile() {
               <p className="inline-block pl-3">NZ citizen </p>
             </li>
           </ul>
-          <div className="flex pt-20 justify-center	items-center	">
+          <div className="flex pt-12 justify-center	items-center	">
             <a
               href="images/ryanscv.pdf"
-              className="rounded inline-flex group relative items-center justify-center px-3.5 py-2 m-1 cursor-pointer border-b-4 border-l-2 active:border-purple-600 active:shadow-none shadow-lg bg-gradient-to-tr from-purple-600 to-purple-500 border-purple-700 text-white"
+              className="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
             >
               <svg
                 className="fill-current w-4 h-4 mr-2"

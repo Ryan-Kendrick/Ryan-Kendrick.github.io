cdfbf0b13e62605053db37a1a31493df6fd37f25
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-07-23 18:26:09 +1200

    add image gallery

--- client/components/Givecredit.tsx
+++ client/components/Givecredit.tsx
@@ -73,56 +73,43 @@ function Givecredit() {
           </div>
           <div data-aos="fade-left" className="flex">
             <div className="basis 2/3 relative place-self-center lg:ml-10 pt-10 md:pt-0">
-              {/* <div className="grid grid-cols-2 gap-2">
-                <div>
-                  <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-                    alt=""
-                  />
-                </div>
-              </div> */}
-
-              <img
-                className="w-full h-full object-center object-cover m-auto"
-                src="images/givecredit2.png"
-                alt="give credit"
-              ></img>
-              <div className="absolute rounded w-3/4 left-0 right-0 mx-auto top-12 md:top-5 py-2 bg-gray-900 shadow-3xl opacity-75">
-                <svg
-                  className="mx-auto my-2 w-10 h-10 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="yellow"
-                    d="M331.8 224.1c28.29 0 54.88 10.99 74.86 30.97l19.59 19.59c40.01-17.74 71.25-53.3 81.62-96.65c5.725-23.92 5.34-47.08 .2148-68.4c-2.613-10.88-16.43-14.51-24.34-6.604l-68.9 68.9h-75.6V97.2l68.9-68.9c7.912-7.912 4.275-21.73-6.604-24.34c-21.32-5.125-44.48-5.51-68.4 .2148c-55.3 13.23-98.39 60.22-107.2 116.4C224.5 128.9 224.2 137 224.3 145l82.78 82.86C315.2 225.1 323.5 224.1 331.8 224.1zM384 278.6c-23.16-23.16-57.57-27.57-85.39-13.9L191.1 158L191.1 95.99l-127.1-95.99L0 63.1l96 127.1l62.04 .0077l106.7 106.6c-13.67 27.82-9.251 62.23 13.91 85.39l117 117.1c14.62 14.5 38.21 14.5 52.71-.0016l52.75-52.75c14.5-14.5 14.5-38.08-.0016-52.71L384 278.6zM227.9 307L168.7 247.9l-148.9 148.9c-26.37 26.37-26.37 69.08 0 95.45C32.96 505.4 50.21 512 67.5 512s34.54-6.592 47.72-19.78l119.1-119.1C225.5 352.3 222.6 329.4 227.9 307zM64 472c-13.25 0-24-10.75-24-24c0-13.26 10.75-24 24-24S88 434.7 88 448C88 461.3 77.25 472 64 472z"
-                  />
-                </svg>
-                <h3 className="tracking-tight leading-none text-xl md:text-2xl xl:text-3xl text-white text-center">
-                  Under Construction
-                </h3>
+              <div className="grid grid-cols-2 gap-2">
+                <a href="images/gc1.jpg">
+                  <div>
+                    <img
+                      className="h-auto max-w-full rounded-lg"
+                      src="images/gc1.jpg"
+                      alt="Give Credit"
+                    />
+                  </div>
+                </a>
+                <a href="images/gc2.jpg">
+                  <div>
+                    <img
+                      className="h-auto max-w-full rounded-lg"
+                      src="images/gc2.jpg"
+                      alt="Give Credit tax calculator"
+                    />
+                  </div>
+                </a>
+                <a href="images/gc3.jpg">
+                  <div className="bg-black rounded-lg">
+                    <img
+                      className="h-auto max-w-full max-h-[430px] rounded-lg m-auto"
+                      src="images/gc3.jpg"
+                      alt="Give Credit on mobile"
+                    />
+                  </div>
+                </a>
+                <a href="images/gc4.jpg">
+                  <div className="bg-black rounded-lg">
+                    <img
+                      className="h-auto max-w-full max-h-[430px] rounded-lg m-auto"
+                      src="images/gc4.jpg"
+                      alt="Give Credit on mobile"
+                    />
+                  </div>
+                </a>
               </div>
             </div>
           </div>

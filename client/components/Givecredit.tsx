07d6303545562a4eb47fa922c39dfd3972174227
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-09-25 14:42:17 +1300

    update give credit deployment

--- client/components/Givecredit.tsx
+++ client/components/Givecredit.tsx
@@ -59,7 +59,7 @@ function Givecredit() {
                     </svg>
                   </span>
                 </form>
-                <form action="https://give-credit.vercel.app/" target="_blank">
+                <form action="https://give-credit.co.nz/" target="_blank">
                   <span className="cursor-pointer inline-block border border-gray-200 bg-gray-200 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:shadow-outline">
                     <input
                       className="cursor-pointer"
